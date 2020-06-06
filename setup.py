b4ae0f902a6c8409726586e12ab49de91ced7b3f
SteveJayH <jay0118@yonsei.ac.kr>
2020-06-06 15:08:16 +0900

    0.0.1

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'tiftool',
+    version = '0.0.1',
+    description = 'tiftool',
+    author = 'Seungjae',
+    author_email = 'jay0118@yonsei.ac.kr',
+    url = 'https://github.com/SteveJayH/tiftool',
+    download_url = 'https://api.github.com/repos/SteveJayH/tiftool/tarball',
+    install_requires = ['tifffile'],  # TODO
+    packages = find_packages(exclude = []),
+    keywords = ['tiftool'],  # TODO
+    python_requires = '>=3',
+    package_data = {},
+    zip_safe = False,
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
