f7631d2e95102aceed296466053b6b22bf9e73cf
SteveJayH <jay0118@yonsei.ac.kr>
2020-06-06 22:50:10 +0900

    0.1.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'tiftool',
-    version = '0.0.2',
+    version = '0.1.1',
     description = 'tiftool',
     author = 'Seungjae',
     author_email = 'jay0118@yonsei.ac.kr',
     url = 'https://github.com/SteveJayH/tiftool',
     download_url = 'https://api.github.com/repos/SteveJayH/tiftool/tarball',
-    install_requires = ['tifffile', 'PIL', 'numpy', 'matplotlib', 'torch'],  # TODO
+    install_requires = ['tifffile', 'Pillow', 'numpy', 'matplotlib', 'torch'],  # TODO
     packages = find_packages(exclude = []),
     keywords = ['tiftool', 'tiff', 'microscopy'],  # TODO
     python_requires = '>=3',
